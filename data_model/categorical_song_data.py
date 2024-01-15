63cacc343e076bb18002252ce2787bea1f48d50d
OZAN\ozand <ozan.demirel@hotmail.com>
2024-01-15 23:13:21 +0300

    Turned Categorical and Numerical Song Data Classes into a form of enums, used for just to set and get the list of these features (to access through other scripts)

--- data_model/categorical_song_data.py
+++ data_model/categorical_song_data.py
@@ -1,48 +1,17 @@
-from song_definition import Song
+class CategoricalSongData:
+    # List of categorical feature names
+    categorical_features = ['title', 'artist', 'lyrics', 'genre', 'main_artist',
+                            'spotify_link', 'spotify_id', 'video_link',
+                            'simple_title', 'analysis_url']
 
+    def __init__(self):
+        pass
 
-class CategoricalSongData(Song):
-    categorical_features = ['title', 'date', 'simple_title', 'artist', 'main_artist', 'spotify_link', 'spotify_id',
-                            'video_link', 'genre', 'analysis_url', 'lyrics']
-
-    def __init__(self, title, artist, peak_pos, lyrics, date, main_artist=None, change=None, spotify_link=None,
-                 spotify_id=None, video_link=None, genre=None, broad_genre=None, analysis_url=None,
-                 additional_spotify_attributes=None):
+    @staticmethod
+    def get_categorical_features():
         """
-        Initialize the CategoricalSongData class with categorical attributes.
-
-        :param title: str - Title of the song.
-        :param artist: str - Artist of the song.
-        :param peak_pos: int - Peak position on the Billboard chart.
-        :param lyrics: str - Lyrics of the song.
-        :param date: str - Date of the song's chart appearance.
-        :param main_artist: str - Main artist of the song.
-        :param change: str - Change in chart position.
-        :param spotify_link: str - Link to the song on Spotify.
-        :param spotify_id: str - Spotify ID of the song.
-        :param video_link: str - Link to the song's video.
-        :param genre: str - Genre of the song.
-        :param broad_genre: str - Broader genre categorization.
-        :param analysis_url: str - URL for detailed song analysis.
-        :param additional_spotify_attributes: dict - Other Spotify attributes.
+        Static method to get the list of categorical features.
+        :return: List of categorical feature names.
         """
-        super().__init__(title, artist, peak_pos)
-        self.lyrics = lyrics
-        self.date = date
-        self.main_artist = main_artist
-        self.change = change
-        self.spotify_link = spotify_link
-        self.spotify_id = spotify_id
-        self.video_link = video_link
-        self.genre = genre
-        self.broad_genre = broad_genre
-        self.analysis_url = analysis_url
-        self.additional_spotify_attributes = additional_spotify_attributes or {}
+        return CategoricalSongData.categorical_features
 
-    def process_lyrics(self):
-        """
-        Process the lyrics for NLP tasks.
-        """
-        # NLP processing logic goes here
-
-    # Additional methods for handling and processing other categorical data can be added here
--- data_model/numerical_song_data.py
+++ data_model/numerical_song_data.py
@@ -1,46 +1,18 @@
-from song_definition import Song
+class NumericalSongData:
+    # List of numerical feature names
+    numerical_features = ['year', 'peak_pos', 'last_pos', 'weeks', 'rank',
+                          'energy', 'liveness', 'tempo', 'speechiness',
+                          'acousticness', 'instrumentalness', 'time_signature',
+                          'danceability', 'key', 'duration_ms', 'loudness',
+                          'valence', 'mode']
 
-class NumericalSongData(Song):
-    numerical_features = ['year', 'peak_pos', 'last_pos', 'weeks', 'rank', 'change', 'energy', 'liveness',
-                          'tempo', 'speechiness', 'acousticness', 'instrumentalness', 'time_signature', 'danceability',
-                          'key', 'duration_ms', 'loudness', 'valence', 'mode']
-    def __init__(self, title, artist, peak_pos, year, last_pos, weeks, rank, spotify_attributes):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_numerical_features():
         """
-        Initialize the NumericalSongData class with numerical attributes.
-
-        :param title: str - Title of the song
-        :param artist: str - Artist of the song
-        :param peak_pos: int - Peak position on the Billboard chart
-        :param year: int - Year the song was on the Billboard chart
-        :param last_pos: int - Last position of the song on the Billboard chart
-        :param weeks: int - Number of weeks the song was on the Billboard chart
-        :param rank: int - Rank of the song
-        :param spotify_attributes: dict - Spotify related attributes
+        Static method to get the list of numerical features.
+        :return: List of numerical feature names.
         """
-        super().__init__(title, artist, peak_pos)
-        self.year = year
-        self.last_pos = last_pos
-        self.weeks = weeks
-        self.rank = rank
-
-        # Process and store Spotify attributes
-        self.process_spotify_attributes(spotify_attributes)
-
-    def process_spotify_attributes(self, attributes):
-        """
-        Process and store Spotify attributes.
-
-        :param attributes: dict - Spotify related attributes
-        """
-        self.acousticness = attributes.get('acousticness')
-        self.danceability = attributes.get('danceability')
-        self.duration_ms = attributes.get('duration_ms')
-        self.instrumentalness = attributes.get('instrumentalness')
-        self.key = attributes.get('key')
-        self.loudness = attributes.get('loudness')
-        self.mode = attributes.get('mode')
-        self.tempo = attributes.get('tempo')
-        self.time_signature = attributes.get('time_signature')
-        self.valence = attributes.get('valence')
-
-        # Additional processing can be added here as needed
+        return NumericalSongData.numerical_features
